bb12845e4d6c589992a565f3b3618aa482622074
daani <aekdusa94@gmail.com>
2024-09-14 00:21:36 +0900

    게시판 글쓰기 저장 진행

--- src/main/java/com/ipad/project/board/QuestionController.java
+++ src/main/java/com/ipad/project/board/QuestionController.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,12 +35,6 @@ public class QuestionController {
 		return "board_List";
 	}
 	
-	@GetMapping("/write")
-	public String write() {
-		System.out.println("write start");
-		return "board_Form";
-	}
-	
 	@GetMapping(value = "/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
@@ -46,5 +43,17 @@ public class QuestionController {
 		return "board_detail";
 	}
 	
+	@GetMapping("/write")
+	public String write() {
+		System.out.println("write start");
+		return "board_Form";
+	} 
+	
+	@PostMapping("/write")
+	public String questionWrite(@RequestParam(value="title") String title,  @RequestParam(value="content") String content) {
+		this.questionService.write(title, content);
+		return "redirect:/board_List";
+		
+	}
 
 }
--- src/main/java/com/ipad/project/board/QuestionService.java
+++ src/main/java/com/ipad/project/board/QuestionService.java
@@ -1,6 +1,6 @@
 package com.ipad.project.board;
 
-
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,25 +19,30 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
-	
+
 	private final QuestionRepository questionRepository;
-	
-	public List<Question> getList(){
+
+	public List<Question> getList() {
 		return this.questionRepository.findAll();
 	}
-	
+
 	public Question getQuestion(Integer id) {
 		Optional<Question> question = this.questionRepository.findById(id);
-		if(question.isPresent()) {
+		if (question.isPresent()) {
 			return question.get();
-		}else {
+		} else {
 			throw new DataNotFoundException("question not found");
 		}
-		
-		
+
 	}
 	
-	
-	
+	public void write(String title, String content) {
+		Question addWrite = new Question();
+		addWrite.setTitle(title);
+		addWrite.setContent(content);
+		addWrite.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(addWrite);
+		
+	}
 
 }
